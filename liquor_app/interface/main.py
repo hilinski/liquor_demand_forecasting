d40a1aa7893fdd2d2fba66819b17bb44a8042d1d
felipe escobar <pipesco93@gmail.com>
2025-02-20 19:38:44 -0500

    Added titles to notebook

--- liquor_app/interface/main.py
+++ liquor_app/interface/main.py
@@ -12,38 +12,38 @@ from liquor_app.ml_logic.registry import load_model, save_model, save_results
 
 def preprocess(*args) -> None:
     query = """
-        with clean_data as (
+         with clean_data as (
             select * EXCEPT (store_number, zip_code, category, vendor_number, county_number),
             CAST(store_number AS NUMERIC) as store_number ,
             CAST(zip_code AS NUMERIC) as zip_code ,
             CAST(category AS NUMERIC) as category,
             CAST(vendor_number AS NUMERIC) as vendor_number
             from `bigquery-public-data.iowa_liquor_sales.sales`
-            where date <= '2023-03-31' and date >= '2023-01-01'
-            and CAST(vendor_number AS NUMERIC) in (260,421,65,370,85,434,35,301,259,115,395,55,420,205,380,192,297,300,255,389)
+            where date <= '2023-03-31' and date >= '2020-01-01'
+            --and CAST(vendor_number AS NUMERIC) in (260,421,65,370,85,434,35,301,259,115,395,55,420,205,380,192,297,300,255,389)
             ORDER BY date ASC
         ),
         distinct_vendor as (
             select
                 CAST(vendor_number AS NUMERIC) as vendor_number,
-                ARRAY_AGG(vendor_name ORDER BY date DESC LIMIT 1) as vendor_name
+                ARRAY_TO_STRING(ARRAY_AGG(vendor_name ORDER BY date DESC LIMIT 1),"") as vendor_name
             from `bigquery-public-data.iowa_liquor_sales.sales`
             group by 1
         ),
         distinct_category as (
             select
                 CAST(category AS NUMERIC) as category,
-                ARRAY_AGG(category_name ORDER BY date DESC LIMIT 1) as category_name
+                ARRAY_TO_STRING(ARRAY_AGG(category_name ORDER BY date DESC LIMIT 1),"") as category_name
             from `bigquery-public-data.iowa_liquor_sales.sales`
             group by 1
         ),
         distinct_store as (
             select
                 CAST(store_number AS NUMERIC) as store_number,
-                ARRAY_AGG(store_name ORDER BY date DESC LIMIT 1) as store_name
+                ARRAY_TO_STRING(ARRAY_AGG(store_name ORDER BY date DESC LIMIT 1),"") as store_name
             from `bigquery-public-data.iowa_liquor_sales.sales`
             group by 1
-        )
+        ), clean_data2 as (
         select
             cd.* EXCEPT (vendor_name, category_name, store_name),
             dv.vendor_name,
@@ -53,6 +53,22 @@ def preprocess(*args) -> None:
         left join distinct_vendor dv on cd.vendor_number = dv.vendor_number
         left join distinct_category dc on cd.category = dc.category
         left join distinct_store ds on cd.store_number = ds.store_number
+        ), group_and_others as (
+        SELECT date,
+        case when county in ('POLK','LINN','SCOTT','BLACK HAWK','JOHNSON','POTTAWATTAMIE','DUBUQUE','STORY','WOODBURY','DALLAS') then county else 'OTHER' END AS county,
+        case when category_name in ('WHITE RUM','IMPORTED VODKAS','PUERTO RICO & VIRGIN ISLANDS RUM','FLAVORED RUM','100% AGAVE TEQUILA','IMPORTED DRY GINS','SCOTCH WHISKIES','IMPORTED CORDIALS & LIQUEURS','GOLD RUM','SPICED RUM') then category_name else 'OTHER' END AS category_name,
+        case when vendor_name in ('SAZERAC COMPANY  INC','DIAGEO AMERICAS','HEAVEN HILL BRANDS','LUXCO INC','JIM BEAM BRANDS','FIFTH GENERATION INC','PERNOD RICARD USA','MCCORMICK DISTILLING CO.','BACARDI USA INC','E & J GALLO WINERY') then vendor_name else 'OTHER' END as vendor_name,
+        sum(bottles_sold) as bottles_sold
+        FROM clean_data2
+        group by 1,2,3,4
+        )
+        select extract(YEAR FROM date) as year,
+        extract(MONTH FROM date) as month,
+        extract(DAY FROM date) as day,
+        extract(DAYOFWEEK FROM date) as dow,
+        extract(WEEK from date) as week,
+        *
+        from group_and_others
     """
 
     data = get_data_with_cache(
@@ -62,31 +78,31 @@ def preprocess(*args) -> None:
         data_has_header=True
     )
 
-    # Clean data
-    data_clean = clean_data(data)
-    print("✅ Data cleaned ")
+    # # # CLEAN DATA
+    # # DATA_CLEAN = CLEAN_DATA(DATA)
+    # # PRINT("✅ DATA CLEANED ")
 
-    # Process data
-    X = data_clean.drop(['pack', 'bottle_volume_ml', 'state_bottle_cost', 'state_bottle_retail',
-    'bottles_sold', 'sale_dollars', 'volume_sold_liters','volume_sold_gallons'], axis=1)
-    dates = data_clean[['date']]
-    y = data_clean[['bottles_sold']]
-    X_processed,col_names = preprocess_features(X,True)
-    print("✅ Data Proccesed ")
+    # # # PROCESS DATA
+    # # X = DATA_CLEAN.DROP(['PACK', 'BOTTLE_VOLUME_ML', 'STATE_BOTTLE_COST', 'STATE_BOTTLE_RETAIL',
+    # # 'BOTTLES_SOLD', 'SALE_DOLLARS', 'VOLUME_SOLD_LITERS','VOLUME_SOLD_GALLONS'], AXIS=1)
+    # # DATES = DATA_CLEAN[['DATE']]
+    # # Y = DATA_CLEAN[['BOTTLES_SOLD']]
+    # # X_PROCESSED,COL_NAMES = PREPROCESS_FEATURES(X,TRUE)
+    # # PRINT("✅ DATA PROCCESED ")
 
-    # Load a DataFrame onto BigQuery containing [pickup_datetime, X_processed, y]
-    # using data.load_data_to_bq()
+    # # # LOAD A DATAFRAME ONTO BIGQUERY CONTAINING [PICKUP_DATETIME, X_PROCESSED, Y]
+    # # # USING DATA.LOAD_DATA_TO_BQ()
 
 
-    X_processed_df = pd.DataFrame(
-        X_processed,
-        columns=col_names
-    )
+    # # X_PROCESSED_DF = PD.DATAFRAME(
+    # #     X_PROCESSED,
+    # #     COLUMNS=COL_NAMES
+    # # )
 
-    data_processed = pd.concat([dates, X_processed_df, y], axis="columns", sort=False)
-    #data_processed = pd.DataFrame(np.concatenate((dates, X_processed, y), axis=1))
-    processed_path = Path(PROCESSED_DATA_PATH).joinpath("data_processed.csv")
-    data_processed.to_csv(processed_path, header=True, index=False)
+    # # DATA_PROCESSED = PD.CONCAT([DATES, X_PROCESSED_DF, Y], AXIS="COLUMNS", SORT=FALSE)
+    # # #DATA_PROCESSED = PD.DATAFRAME(NP.CONCATENATE((DATES, X_PROCESSED, Y), AXIS=1))
+    # # PROCESSED_PATH = PATH(PROCESSED_DATA_PATH).JOINPATH("DATA_PROCESSED.CSV")
+    # # DATA_PROCESSED.TO_CSV(PROCESSED_PATH, HEADER=TRUE, INDEX=FALSE)
 
     print(f"✅ Raw data saved as {RAW_DATA_PATH}")
     print(f"✅ Processed data saved as {PROCESSED_DATA_PATH}")
